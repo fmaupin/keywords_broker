714cd77ed57d72977d5b1760555134d4a025a9e9
fmaupin <fabricemaupin@gmail.com>
2025-12-22 18:39:50 +0100

    [Config] add qcompleted queue

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,5 @@ docker container stop $(docker ps -aq) || true
 docker container rm $(docker ps -aq)
 
 # instantiation de l'image rabbit-keywords:latest
-docker run -d --hostname rabbit-keywords --name rabbit-keywords -p 5672:5672 -p 16000:16000 rabbit-keywords:latest  
+docker run -d --hostname rabbit-keywords --name rabbit-keywords -p 5672:5672 -p 15672:15672 rabbit-keywords:latest  
 
